d9ae2fac3bdeb7e7075ac5024f14ae4df95e4856
davidkimgba <44686023+davidkimgba@users.noreply.github.com>
2023-04-03 05:57:46 -0700

    Need to implement WebDriverWait

--- Scrape.py
+++ Scrape.py
@@ -1,40 +1,64 @@
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select # Used in dropdown to selections
 from selenium.webdriver.common.by import By
-import time
+import time # to wait for page loading
+import getpass # to conceal password
+
+def loginPrompt():
+    userNameField = browser.find_element(By.ID, "ssousername")
+    userNameField.send_keys(input("Enter SSO username\n"))
+
+    passwordField = browser.find_element(By.ID, 'ssopassword')
+    passwordField.send_keys(getpass.getpass("Enter SSO password (hidden input field) \n"))
+
+    #press login button
+    browser.find_element(By.CLASS_NAME, "btn.btn-primary.pull-right.sso-button").click()
+    # pageLoading = True
+    # while pageLoading :
+    #     try:
+    #         browser.find_element(By.ID, "duo_iframe").is_displayed
+    #     except NoSuchElementException:
+
+    while browser.find_element(By.ID, "duo_iframe").is_displayed or browser.find_element(By.ID, "_login_error_message").is_displayed:
+        time.sleep(0.1)
+        print(browser.find_element(By.ID, "duo_iframe").is_displayed)
+        print("promptloop")
+
+    if browser.find_element(By.ID, "_login_error_message").is_displayed:
+        return False
+    else:
+        return True
+    
 
 
-#Initialize URL
 URL = "https://cape.ucsd.edu/responses/Results.aspx"
 browser = webdriver.Chrome()
 browser.get(URL)
 
-# userNameField = browser.find_element(By.ID, "ssousername")
-# userNameField.send_keys(input("Enter SSO username\n"))
+while(not loginPrompt()):
+    print("loginloop")
+    print(browser.find_element(By.ID, "_login_error_message").text)
 
-# passwordField = browser.find_element(By.ID, 'ssopassword')
-# passwordField.send_keys(input("Enter SSO password\n"))
-time.sleep(5)
-while browser.current_url!="https://cape.ucsd.edu/responses/Results.aspx":
-    time.sleep(1)
+#Dealing with iframes https://stackoverflow.com/questions/24247490/find-elements-inside-forms-and-iframe-using-java-and-selenium-webdriver
+#Switch to iframe on selenium driver
+browser.switch_to.frame('duo_iframe')
+
+#Waits on browser duo iframe to load by checking if the text next to the dropdown is rendered
+#(Required because dropdown options text will not output correctly otherwise)
+while browser.find_element(By.CLASS_NAME, "cramped-frame-view").text != "Device:":
+    time.sleep(0.5)
+
+# convert element into selection dropdown
+duoDeviceDropDown = Select(browser.find_element(By.TAG_NAME, 'select'))
+#change dropdown into options list and iterate
+print("Duo authentication device options (type the number to select)")
+print("↓ [default] ↓")
+
+# convert dropdown selections to list of options and iterate, convert devicecount to string before printing
+deviceCount = 1
+for device in duoDeviceDropDown.options:
+    print("(" + str(deviceCount) + ") " +  device.text + "\n")
+    deviceCount+=1
 
 
-URL = browser.current_url
-
-#http request to get page
-page = requests.get(URL)
-#Open file to write
-webpageFile = open("capes.html", 'w')
-webpageFile.write(page.text)
-#Open file to read and print to console
-webpageFile = open("capes.html", 'r')
-print(webpageFile.read())
-
-
-#https://stackoverflow.com/questions/11892729/how-to-log-in-to-a-website-using-pythons-requests-module
-#check how to login and get page
-
-
-webpageFile.close()
+time.sleep(1000)
