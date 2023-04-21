c8fd8a33ac04a494f5603709328e62153003b1b4
davidkimgba <davidkimgba@gmail.com>
2023-04-21 11:46:06 -0700

    Creating basic login window

--- ScraperGUI.py
+++ ScraperGUI.py
@@ -2,17 +2,26 @@ from tkinter import *
 
 rootWindow = Tk()
 rootWindow.title("CAPEScraper")
-rootWindow.geometry((800,800))
-
-
-rootFrame = Frame(rootWindow)
-rootButton = Button(rootFrame, text="test")
+rootWindow.geometry('800x800')
 
 
 
 
+topFrame = Frame(rootWindow)
+topFrame.pack(side=TOP)
+rootButton = Button(topFrame, text="test")
+rootButton.pack(side=BOTTOM)
+
+bottomFrame = Frame(rootWindow)
+bottomFrame.pack(side=BOTTOM)
+saveLoginBox = Checkbutton(bottomFrame, text="remember login?")
+saveLoginBox.pack(side=BOTTOM)
+
+saveLoginBox = Checkbutton(bottomFrame, text="remember login?1")
+saveLoginBox.pack(side=BOTTOM)
 
 
+#Check out pynacl for user login info hashing
 
 
 rootWindow.mainloop()
